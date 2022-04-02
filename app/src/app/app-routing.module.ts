8a16437d50c459adaba1f7c3cb43bd0fe9f973b3
Lina Rabenandrasana <madresyrab@gmail.com>
2022-04-02 13:02:57 +0300

    Removing Node Modules

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { LoginComponent } from './pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+	path:'accueil',
+	component:AccueilComponent
+},{
+	path:'login',
+	component:LoginComponent
+},{
+	path:'**',
+	redirectTo:'/accueil'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile('index.html');
+  res.sendFile('index.html',{root:'./public/app'});
 });
 
 module.exports = router;
