d73fb2a3bc3441b750e0be26b9e4f6078481b1ca
Lina Rabenandrasana <madresyrab@gmail.com>
2022-04-10 17:54:52 +0300

    Correction deployement master

--- app/src/app/services/component/position-stack.service.ts
+++ app/src/app/services/component/position-stack.service.ts
@@ -12,6 +12,7 @@ export class PositionStackService {
 	) { }
 
 	search(placename: string) {
+		//TODO : avadika an'ilay api eo ambony no antsoina rehefa vita tanteraka ilay projet
 		//return this.http.get(`http://api.positionstack.com/v1/forward?access_key=${environment.positionStackAPIKey}&query=${placename}`);
 		return this.http.get('localhost:3000');
 	}
